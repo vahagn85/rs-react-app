615babf9253f9ef5b94ed55c0188dc92d667b8b2
vahagn85 <v.ulikhanyan@yandex.ru>
2025-07-17 13:52:07 +0400

    feat: add coverage for test with config

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,25 @@ export default defineConfig({
     globals: true,
     setupFiles: 'vitest.setup.ts',
     include: ['src/**/*.{test,spec}.{ts,tsx}'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{ts,tsx}',
+        'src/**/*.spec.{ts,tsx}',
+        'src/test-utils/**',
+        'src/types/**',
+        'src/main.tsx',
+        'src/config.ts',
+        'src/**/*.d.ts',
+      ],
+      thresholds: {
+        statements: 80,
+        lines: 50,
+        functions: 50,
+        branches: 50,
+      },
+    },
   },
 });
