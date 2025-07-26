154b9e4a11bd1bc27c591aa85cc70fe03e0531f9
vahagn85 <v.ulikhanyan@yandex.ru>
2025-07-26 17:23:20 +0400

    feat: implement not found page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,24 @@
+import { useNavigate } from 'react-router';
+import Button from '../components/Button';
+
 function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/', { replace: true });
+  };
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-68px)] bg-gray-100 text-gray-800 px-4 text-center">
+      <h1 className="text-8xl font-bold mb-4 text-indigo-600 text-shadow-lg">
+        404
+      </h1>
+      <h2 className="text-3xl font-semibold mb-2">Page Not Found</h2>
+      <p className="text-lg mb-6 max-w-md">
+        Oops! The page you&apos;re looking for doesn&apos;t exist or has been
+        moved.
+      </p>
+      <Button name="Go to Homepage" onClick={handleClick} />
+    </div>
+  );
 }
 
 export default NotFoundPage;
