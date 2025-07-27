35309af460034e46ebcf0458a69f3b96ec2040c0
vahagn85 <v.ulikhanyan@yandex.ru>
2025-07-27 20:07:25 +0400

    test: add tests for utils

--- src/test-utils/mockdata/planets.ts
+++ src/test-utils/mockdata/planets.ts
@@ -1,6 +1,19 @@
 import type { Result } from '../../types/result.types';
 
-export const mockPlanets: Result[] = [
+export const mockPlanets: Partial<Result>[] = [
   { name: 'Earth', climate: 'Temperate' },
   { name: 'Mars', climate: 'Arctic' },
 ];
+
+export const mockPlanet: Result = {
+  name: 'Mars',
+  rotation_period: '121',
+  orbital_period: '2524',
+  diameter: '10465',
+  climate: 'arid',
+  gravity: '1 standard',
+  terrain: 'desert',
+  surface_water: '1',
+  population: '200000',
+  url: 'https://test.test/1/',
+};
