c9d8c77b5cc49e6b874cef5e93261a1921e99500
vahagn85 <v.ulikhanyan@yandex.ru>
2025-08-01 16:48:48 +0400

    fix: update formatNumber and his test

--- src/utils/__tests__/planetUtils.test.ts
+++ src/utils/__tests__/planetUtils.test.ts
@@ -4,7 +4,7 @@ import { mockPlanet } from '../../test-utils/mockdata/planets';
 describe('planetUtils Util', () => {
   describe('formatNumber', () => {
     it('should format a number toLocaleString', () => {
-      expect(formatNumber('1000000')).toBe('1\u00A0000\u00A0000');
+      expect(formatNumber('1000000')).toBe('1,000,000');
     });
 
     it('should return "Unknown" when input is "unknown"', () => {
@@ -19,12 +19,12 @@ describe('planetUtils Util', () => {
       expect(details).toEqual([
         { label: 'Rotation Period', value: '121 hours' },
         { label: 'Orbital Period', value: '2524 days' },
-        { label: 'Diameter', value: '10\u00A0465 km' },
+        { label: 'Diameter', value: '10,465 km' },
         { label: 'Climate', value: 'arid' },
         { label: 'Gravity', value: '1 standard' },
         { label: 'Terrain', value: 'desert' },
         { label: 'Surface Water', value: '1%' },
-        { label: 'Population', value: '200\u00A0000' },
+        { label: 'Population', value: '200,000' },
       ]);
     });
   });
