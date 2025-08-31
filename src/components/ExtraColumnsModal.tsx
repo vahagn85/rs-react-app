a1245a031fcd24cc9e25c342036c05bc1accc703
vahagn85 <v.ulikhanyan@yandex.ru>
2025-08-31 14:20:10 +0200

    refactor: update ExtraColumns modal, optimize

--- src/components/ExtraColumnsModal.tsx
+++ src/components/ExtraColumnsModal.tsx
@@ -1,5 +1,5 @@
-import type { Dispatch, SetStateAction } from 'react';
-import type { ExtraColumns } from '../types/CountriesType';
+import { useState, type Dispatch, type SetStateAction } from 'react';
+import type { ExtraColumns } from '../types/countriesType';
 import { extraFields } from '../data/fields';
 
 interface ExtraColumnsModalProps {
@@ -13,6 +13,16 @@ function ExtraColumnsModal({
   setExtraColumns,
   onClose,
 }: ExtraColumnsModalProps) {
+  const [selectedColumns, setSelectedColumns] = useState(extraColumns);
+  const toggleColumn = (field: ExtraColumns) => {
+    setSelectedColumns((prev) =>
+      prev.includes(field) ? prev.filter((f) => f !== field) : [...prev, field]
+    );
+  };
+  const handleApply = () => {
+    setExtraColumns(selectedColumns);
+    onClose();
+  };
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-80">
@@ -22,23 +32,20 @@ function ExtraColumnsModal({
             <label key={field} className="flex items-center space-x-2">
               <input
                 type="checkbox"
-                checked={extraColumns.includes(field as ExtraColumns)}
-                onChange={() =>
-                  setExtraColumns((prev) =>
-                    prev.includes(field as ExtraColumns)
-                      ? prev.filter((f) => f !== field)
-                      : [...prev, field as ExtraColumns]
-                  )
-                }
+                checked={selectedColumns.includes(field as ExtraColumns)}
+                onChange={() => toggleColumn(field as ExtraColumns)}
               />
               <span>{field}</span>
             </label>
           ))}
         </div>
         <div className="mt-4 flex justify-end space-x-2">
+          <button className="px-3 py-1 bg-gray-300 rounded" onClick={onClose}>
+            Cancel
+          </button>
           <button
             className="px-3 py-1 bg-blue-500 text-white rounded"
-            onClick={onClose}
+            onClick={handleApply}
           >
             Apply
           </button>
